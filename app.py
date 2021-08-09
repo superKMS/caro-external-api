ca5d56c25391ac6a5a4784fd9ceaa674a6118a13
superKMS <alstjddl8@naver.com>
2021-08-09 09:50:14 +0900

    test

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+import requests
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
@@ -7,7 +11,13 @@ def hello():
 
 @app.route("/car-list")
 def car_list():
-    return "car-list"
+    url = 'https://fms-dev-api.carbom.co.kr/fms/v1/caro/car'
+    response = requests.get(url,
+                            headers = {"accept": "application/json; charset=utf-8", "Content-Type": "application/json; charset=utf-8",
+                                         "Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYXJvQGNhcm8uY28ua3IiLCJhdWQiOiJhdXRoVG9rZW47Rk1TVXNlcjsiLCJpYXQiOjE2MjYyNDc0MTEsImV4cCI6MTYyODgzOTQxMSwiY29tcGFueV9pZCI6MTA0LCJicmFuY2hfaWQiOjQwMn0.KEeSQ4BIMNIXXapF7zCkXGVwYDydlejuS3bkZoZQIsw"},
+                            )
+
+    return response.status_code
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
